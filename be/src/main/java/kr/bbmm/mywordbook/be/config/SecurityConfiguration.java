a2bfc07039e169d39437181f504e405340551130
devdinky <devdinky@gmail.com>
2020-09-17 13:05:13 +0900

    mywordbook-3 Add JWT and Security config

--- be/src/main/java/kr/bbmm/mywordbook/be/config/SecurityConfiguration.java
+++ be/src/main/java/kr/bbmm/mywordbook/be/config/SecurityConfiguration.java
@@ -1,19 +1,40 @@
 package kr.bbmm.mywordbook.be.config;
 
+import kr.bbmm.mywordbook.be.config.jwt.JWTConfigurer;
+import kr.bbmm.mywordbook.be.config.jwt.TokenProvider;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    private final TokenProvider tokenProvider;
+
+    public SecurityConfiguration(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http
+            // Disable csrf because using JWT
+            .csrf()
+                .disable()
+
             .headers()
             .contentSecurityPolicy("default-src 'self'; frame-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:")
         .and()
@@ -28,10 +49,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .frameOptions()
             .deny()
         .and()
+            // Don't use session because using JWT
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
             .authorizeRequests()
             // Add requests Here
 
         .and()
-            .httpBasic();
+            .httpBasic()
+        .and()
+            .apply(securityConfigurerAdapter())
+        ;
     }
+
+    private JWTConfigurer securityConfigurerAdapter() {
+        return new JWTConfigurer(tokenProvider);
+    }
+
 }
