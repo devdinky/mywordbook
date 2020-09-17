5083d54989a9a4119fd98e9d45f2ad26d097008f
devdinky <devdinky@gmail.com>
2020-09-17 18:13:56 +0900

    mywordbook-23 Add controller advice

---
+++ be/src/main/java/kr/bbmm/mywordbook/be/controller/ControllerExceptionHandler.java
@@ -0,0 +1,14 @@
+package kr.bbmm.mywordbook.be.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.zalando.problem.spring.web.advice.ProblemHandling;
+import org.zalando.problem.spring.web.advice.security.SecurityAdviceTrait;
+
+@ControllerAdvice
+public class ControllerExceptionHandler implements ProblemHandling, SecurityAdviceTrait {
+
+    private static final Logger log = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+
+}
