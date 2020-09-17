40e7a72dc7eb2e29a9aaf81f511f43c1e769e877
devdinky <devdinky@gmail.com>
2020-09-17 16:50:45 +0900

    mywordbook-16 Add check user id and email duplication

--- be/src/main/java/kr/bbmm/mywordbook/be/repository/UserRepository.java
+++ be/src/main/java/kr/bbmm/mywordbook/be/repository/UserRepository.java
@@ -4,6 +4,13 @@ import kr.bbmm.mywordbook.be.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findOneByEmailIgnoreCase(String email);
+
+    Optional<User> findOneByLogin(String login);
+
 }
